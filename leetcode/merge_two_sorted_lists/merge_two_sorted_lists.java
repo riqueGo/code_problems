aa369a358aa076f1e4a43f9816b079ac5a69695f
henrique.oliveira.gomes@gmail.com <henrique.oliveira.gomes@gmail.com>
2025-06-07 18:51:58 -0300

    merge_two_sorted_lists

---
+++ leetcode/merge_two_sorted_lists/merge_two_sorted_lists.java
@@ -0,0 +1,39 @@
+package merge_two_sorted_lists;
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+
+        while(list1 != null && list2 != null) {
+            if(list1.val < list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        if(list1 != null) {
+            curr.next = list1;
+        }
+
+        if(list2 != null) {
+            curr.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode l = new Solution().mergeTwoLists(l1, l2);
+        while(l != null) {
+            System.out.print(l.val + " ");
+        }
+    }
+
+}
