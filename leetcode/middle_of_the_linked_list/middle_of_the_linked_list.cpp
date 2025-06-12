40f8ef9958c1c25a252cc3627146647803c7958a
henrique.oliveira.gomes@gmail.com <henrique.oliveira.gomes@gmail.com>
2025-06-12 12:11:03 -0300

    middle_of_the_linked_list

---
+++ leetcode/middle_of_the_linked_list/middle_of_the_linked_list.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* fast = head->next;
+        while(fast != nullptr) {
+            head = head->next;
+            fast = fast->next;
+            if(fast == nullptr) return head;
+            fast = fast->next;
+        }
+        return head;
+    }
+};
---
+++ leetcode/middle_of_the_linked_list/middle_of_the_linked_list.java
@@ -0,0 +1,24 @@
+package middle_of_the_linked_list;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head.next;
+        while(fast!=null) {
+            head = head.next;
+            fast = fast.next;
+            if(fast == null) return head;
+            fast = fast.next;
+        }
+        return head;
+    }
+}
