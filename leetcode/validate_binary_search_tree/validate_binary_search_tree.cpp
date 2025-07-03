8d6660d08e314dd71e88fb6333ffcf05bb18b570
henrique.oliveira.gomes@gmail.com <henrique.oliveira.gomes@gmail.com>
2025-07-03 19:08:59 -0300

    validate_binary_search_tree

---
+++ leetcode/validate_binary_search_tree/validate_binary_search_tree.cpp
@@ -0,0 +1,32 @@
+#include <climits>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return dfs(root, LONG_MIN, LONG_MAX);
+    }
+
+    bool dfs(TreeNode* root, long min, long max) {
+        return root == nullptr || (root->val > min && root->val < max && dfs(root->left, min, root->val) && dfs(root->right, root->val, max));
+    }
+};
---
+++ leetcode/validate_binary_search_tree/validate_binary_search_tree.java
@@ -0,0 +1,39 @@
+package validate_binary_search_tree;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean dfs(TreeNode root, long min, long max) {
+        return root == null || (root.val > min && root.val < max && dfs(root.left, min, root.val) && dfs(root.right, root.val, max));
+    }
+}
